08bb52f7eff71b68d605f5dbf995df963589c960
William Shinji <willshinji@gmail.com>
2025-01-13 09:35:18 +0000

    Create utils.js

---
+++ SimpleLibrary/utils.js
@@ -0,0 +1,12 @@
+function greet(name) {
+    return `Hello, ${name}!`;
+  }
+  
+  function farewell(name) {
+    return `Goodbye, ${name}!`;
+  }
+  
+  module.exports = {
+    greet,
+    farewell
+  };
